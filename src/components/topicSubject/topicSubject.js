c464f075b3b475c3b3e996728e8a467e81187db5
unknown <castrorodrigo355@gmail.com>
2021-01-03 23:57:18 -0300

    cualquier_cosa

--- src/components/topicSubject/topicSubject.js
+++ src/components/topicSubject/topicSubject.js
@@ -1,6 +1,16 @@
 import React from "react";
+import { GiClick } from "react-icons/gi";
 import "./topicSubject.css";
 
 export const TopicSubject = ({ topic }) => {
-  return <div className="topic--subject--main--container">{topic.topic}</div>;
+  const clickTopic = () => console.log(topic);
+
+  return (
+    <div className="topic--subject--main--container">
+      <div className="topic--subject--main--container--name">{topic.topic}</div>
+      <div className="topic--subject--main--container--icon">
+        <GiClick onClick={clickTopic} />
+      </div>
+    </div>
+  );
 };
